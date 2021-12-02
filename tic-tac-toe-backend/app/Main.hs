cacea04ae62af363cc6e669430f4683450978a95
Jordan Medlock <jordanemedlock@gmail.com>
2021-12-01 17:53:52 -0800

    Scotty hello world

--- tic-tac-toe-backend/app/Main.hs
+++ tic-tac-toe-backend/app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import qualified Web.Scotty.Trans as S
+import Data.Text.Lazy (Text)
 
 main :: IO ()
-main = someFunc
+main = S.scottyT 5000 id routes
+
+
+routes :: S.ScottyT Text IO ()
+routes = do
+    S.get "/1/" $ do
+        S.text "Hello World!"
